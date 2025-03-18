9e4342c66d2cc85f3513234465d2e4e55949d4f5
Tibidinu1031 <132930615+Tibidinu1031@users.noreply.github.com>
2025-03-18 19:40:20 +0200

    Add files via upload

---
+++ Day 6 - co-founder roles.py
@@ -0,0 +1,12 @@
+print("MY LOGIN SYSTEM")
+print("+++++++++++++++")
+whichcofounder = input("Username > ")
+password = input("Password > ")
+if whichcofounder == "Tiberius Dinu" and password == "cofounder&CEONeoteq": 
+  print("Welcome Tiberius Dinu, you are the cofounder and CEO of Neoteq. Since you are in charge of growth, vision and R%D, make sure to keep learning, adapting and iterating.")
+elif whichcofounder == "future co-founder & CSO" and password == "neuroscience":
+  print("Hi there co-founder and CSO, since this is a neuroscience-based startup - wouldn't that make you the brain of the operation?")
+elif whichcofounder == "future co-founder & CTO" and password == "tech&AI":
+  print("Hi there co-founder and CTO, since this is a tech neuroscience-based company, your technical abilities make and keep this company going, just to let you know!")
+else:
+  print("Who the fuck are you?!?!?!?!?!?!?!?!?!?!?!?!?!?!")
