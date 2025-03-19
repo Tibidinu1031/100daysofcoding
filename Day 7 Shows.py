56511d80fccbdf9a0779123bad8b39724e847b3a
Tibidinu1031 <132930615+Tibidinu1031@users.noreply.github.com>
2025-03-19 18:52:39 +0200

    Add files via upload

---
+++ Day 7 Shows.py
@@ -0,0 +1,23 @@
+print("TV Show Qweez")
+print("")
+tvShow = input("What's ya fav TV Showww? > ")
+if tvShow == "Band of Brothers":
+  throughGunfire = input("Who went through gunfire? > ")
+  if throughGunfire == "Speirs":
+    whichEp = input("In which episode? > ")
+    if whichEp == "7":
+      print("You're a true Band of Brothers fan!")
+elif tvShow == "Better Call Saul":
+  youHaveRights = input("Do you know that you have rights? > ")
+  if youHaveRights == "Yes":
+    print("Better Call Saul!!!")
+  else:
+    print("Better Call Saul!!!")
+else:
+  testHere = input("You sure? > ")
+  if testHere == "Yes":
+    print("Who tf cares?!?!")
+  elif testHere == "No":
+    print("No worries")
+  else:
+    print("Fuggetaboutit!")
