43d236c54e032fbbdbd002f2e124257df9ab15c6
Tibidinu1031 <132930615+Tibidinu1031@users.noreply.github.com>
2025-03-19 18:54:55 +0200

    Add files via upload

---
+++ Day 5 - GoT faith.py
@@ -0,0 +1,26 @@
+print("What's your faith in the GoT realm!")
+print("=========================")
+the_seven = input("Do you believe in the Seven? ")
+if the_seven == "yes":
+  print("Wrong")
+else: print("Correct Answer")
+print("")
+old_gods = input("Do you believe in the Old Gods? ")
+if old_gods == "yes":
+  print("Wrong")
+else: print("Correct Answer")
+print("")
+old_gods = input("Do you believe in the New Gods? ")
+if old_gods == "yes":
+  print("Wrong")
+else: print("Correct Answer")
+print("")
+old_gods = input("Do you believe in the Lord of Light? ")
+if old_gods == "yes":
+  print("Wrong")
+else: print("Correct Answer")
+print("")
+manyFacedGod = input("Do you believe there's only one God and it's all the same to him?: ")
+if manyFacedGod == "no":
+  print("Wrong")
+else: print("Then you're a Faceless Man and you belong to The House of Black and White")
