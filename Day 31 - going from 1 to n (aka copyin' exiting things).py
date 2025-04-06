92861a1e4e8cbfad9ddf2af9f3e05b1e68409f3c
Tibidinu1031 <132930615+Tibidinu1031@users.noreply.github.com>
2025-04-06 19:45:22 +0300

    Add files via upload

---
+++ Day 31 - going from 1 to n (aka copyin' exiting things).py
@@ -0,0 +1,28 @@
+i = "\033[31m=\033[0m=\033[32m=\033[0m  Music App  \033[31m=\033[0m=\033[32m=\033[0m"
+print(f"{i: ^145}")
+print("🔥▶", end="  ")
+print("Radio Gaga")
+queen = "Queen"
+print(f"\033[1;33m{queen: >10}\033[0m")
+print("")
+print("")
+print("PREV")
+next = "NEXT"
+pause = "PAUSE"
+print(f"\033[32m{next: >9}\033[0m")
+print(f"\033[31m{pause: >15}\033[0m")
+print("---------------------------------------------------------------------------")
+print("", end = "                                                  ")
+print("WELCOME TO")
+
+tagline = "--    ARMBOOK    --"
+print(f"\033[38;5;27m{tagline: ^110}\033[0m")
+print("", end="                 ")
+print("""        \033[93m                     Definitely not a rip off of                                                                                      a certain other social                                                                                            networking site.\033[0m""")
+honest = "Honest."
+print(f"\033[31m{honest: ^110}\033[0m")
+username = "Username:"
+print(f"{username: ^110}", end= "                                                ")
+print("Password:")
+
+
