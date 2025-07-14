be4ed4621bc1daa39e44314234ccea9d35060ad8
Tibidinu1031 <132930615+Tibidinu1031@users.noreply.github.com>
2025-07-14 19:37:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,76 @@
-import csv 
+import os
 
-total = 0.0
+print("Personal Expense Project")
+print("------------------------")
+print()
 
-with open("Day54Totals.csv") as file:
-  reader = csv.DictReader(file)
-  for row in reader:
-    total += float(row["Cost"]) * float(row["Quantity"])
+transactions = []
+fileExists = True
+try:
+    f = open("expense.tracker", "r")
+    transactions = eval(f.read())
+    f.close()
+except:
+    print()
+    print("If you are seeing this line it means that you have not made any transactions nor removed one so far.\n Once you do, it'll no longer show")
+    print()
+    fileExists = False
 
-print(f"Total = {round(total, 2)}")
+transactionNumber = 1
+total = 0
+
+def prettyPrint():
+    print()
+    print(f"{'Transaction #':^30} | {'Amount':^30} | {'Category':^30} | {'Note':^30} |")
+    for rows in transactions:
+        for items in rows:
+            print(f"{str(items):^30}", end=" | ")
+        print()
+    print()
+
+while True:
+    pick = input("1. Add new transaction\n2. View the transactions so far\n3. View total\n> ")
+    if pick == "1":
+        print(f"Transaction number: {transactionNumber}")
+        amount = input("What's the value of this transaction? > ")
+        total += int(amount)
+        category = input("What category does this transaction fall into? > ")
+        addNote = input("Add a note?\nY/N\n> ")
+        if addNote.lower() == "y":
+            note = input("What are the details for this transaction? > ")
+            print("Details added!")
+            print()
+            transactionLine = [transactionNumber, amount, category, note]
+            transactions.append(transactionLine)
+        else:
+            note = "No Details"
+            print("This transaction will have not details.")
+            print()
+            transactionLine = [transactionNumber, amount, category, note]
+            transactions.append(transactionLine)
+        prettyPrint()
+        transactionNumber += 1
+    if pick == "2":
+        if transactions != []:
+            prettyPrint()
+            print()
+        else:
+            print("--- Empty list. Pleade log a transcation! ---")
+            print()
+    if pick == "3":
+        print(f"Total Expenses = {total}$")
+        print()
+
+    if fileExists:
+      try:
+        os.mkdir("backup")
+      except:
+        pass
+      backup_file = "expense.tracker"
+      os.system(f"cp expense.tracker backup/{backup_file}")
+
+    f = open("expense.tracker", "w")
+    f.write(str(transactions))
+    f.close()
+
+ 
