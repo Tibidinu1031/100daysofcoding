14183182a6378c98580e3559a1ae6d3aa86fb839
Tibidinu1031 <132930615+Tibidinu1031@users.noreply.github.com>
2025-09-30 19:24:47 +0300

    Add files via upload

---
+++ Day 77/main.py
@@ -0,0 +1,67 @@
+from flask import Flask, redirect
+import datetime
+
+
+app = Flask(__name__, static_url_path="/static")
+
+@app.route("/neoteq")
+def neoteq():
+  return redirect("https://www.neoteq.us")
+
+@app.route("/")
+def index():
+  now = datetime.datetime.now().date()
+  now = str(now)
+  page = """
+  <html>
+  <body>
+  <h1>Hello World!</h1>
+  <h2>{now}</h2>
+  <h3>Here's my first blog entry</h3>
+  <h3><a href = "/blog/pageone" target="_blank">Link to blog post 1</a></h3>
+  <h3><a href = "/blog/pagetwo">Link to blog post 2</a></h3>
+  </body>
+  </html>
+  """
+  page = page.replace("{now}", now)
+  return page
+
+
+@app.route("/pageone")
+def pageone():
+  number = "1111111111111111111"
+  plm = "plm"
+  title = "plm1"
+  page = ""
+  f = open("templates/index.html", "r")
+  page = f.read()
+  f.close()
+  page = page.replace("{number}", number)
+  page = page.replace("{plm}", plm)
+  page = page.replace("{title}", title)
+  return page
+
+@app.route("/blog/pageone")
+def blogpageone():
+    return redirect("/pageone")
+
+
+@app.route("/pagetwo")
+def pagetwo():
+  number = "2222222222222222"
+  title = "plm2"
+  page = ""
+  f = open("templates/index2.html", "r")
+  page = f.read()
+  f.close()
+  page = page.replace("{number}", number)
+  page = page.replace("{title}", title)
+  return page
+
+
+@app.route("/blog/pagetwo")
+def blogpagetwo():
+    return redirect("/pagetwo")
+
+
+app.run(host = "127.0.0.1", port = 5500, debug = True)
