a216569e80c380fe1c0f2dd53fe8131428a92591
Tibidinu1031 <132930615+Tibidinu1031@users.noreply.github.com>
2025-10-14 18:41:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,116 +1,22 @@
-from flask import Flask, redirect, request
-import datetime
+import requests, json, os
 
+result = requests.get("https://randomuser.me/api/")
 
-app = Flask(__name__, static_url_path="/static")
+data = result.json()
 
-@app.route("/neoteq")
-def neoteq():
-  return redirect("https://www.neoteq.us")
+#print(json.dumps(data, indent=3))
 
-@app.route("/", methods=["GET"])
-def index():
-  theme = request.args.get("theme", "")
-  if theme.lower() == "green":
-    return """
-  <html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/static/green.css">
-</head>
-  <body>
-  <h1>Hello World!</h1>
-  <h2>{now}</h2>
-  <h3>Here's my first blog entry</h3>
-  <h3><a href = "/blog/pageone" target="_blank">Link to blog post 1</a></h3>
-  <h3><a href = "/blog/pagetwo">Link to blog post 2</a></h3>
-  </body>
-  </html>
-  """
-  if theme.lower() == "blue":
-    return """
-  <html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/static/blue.css">
-</head>
-  <body>
-  <h1>Hello World!</h1>
-  <h2>{now}</h2>
-  <h3>Here's my first blog entry</h3>
-  <h3><a href = "/blog/pageone" target="_blank">Link to blog post 1</a></h3>
-  <h3><a href = "/blog/pagetwo">Link to blog post 2</a></h3>
-  </body>
-  </html>
-  """
-  if theme.lower() == "default":
-    return """
-  <html>
-  <body>
-  <h1>Hello World!</h1>
-  <h2>{now}</h2>
-  <h3>Here's my first blog entry</h3>
-  <h3><a href = "/blog/pageone" target="_blank">Link to blog post 1</a></h3>
-  <h3><a href = "/blog/pagetwo">Link to blog post 2</a></h3>
-  </body>
-  </html>
-  """
-  now = datetime.datetime.now().date()
-  now = str(now)
-  page = """
-  <html>
-  <body>
-  <h1>Hello World!</h1>
-  <h2>{now}</h2>
-  <h3>Here's my first blog entry</h3>
-  <h3><a href = "/blog/pageone" target="_blank">Link to blog post 1</a></h3>
-  <h3><a href = "/blog/pagetwo">Link to blog post 2</a></h3>
-  </body>
-  </html>
-  """
-  page = page.replace("{now}", now)
-  return page
+name = f'{data['results'][0]['name']['first']} {data['results'][0]['name']['last']}'
 
+image = data['results'][0]['picture']['large']
 
-@app.route("/pageone")
-def pageone():
-  number = "1111111111111111111"
-  plm = "plm"
-  title = "plm1"
-  page = ""
-  f = open("templates/index.html", "r")
-  page = f.read()
-  f.close()
-  page = page.replace("{number}", number)
-  page = page.replace("{plm}", plm)
-  page = page.replace("{title}", title)
-  return page
+picture = requests.get(image)
 
-@app.route("/blog/pageone")
-def blogpageone():
-    return redirect("/pageone")
+for person in data['results']:   
+    f = open(f'poze/poza.jpg', 'wb')
+    f.write(picture.content)
+    index += 1
+    f.close()
+    
 
-
-@app.route("/pagetwo")
-def pagetwo():
-  number = "2222222222222222"
-  title = "plm2"
-  page = ""
-  f = open("templates/index2.html", "r")
-  page = f.read()
-  f.close()
-  page = page.replace("{number}", number)
-  page = page.replace("{title}", title)
-  return page
-
-
-@app.route("/blog/pagetwo")
-def blogpagetwo():
-    return redirect("/pagetwo")
-
-
-app.run(host = "127.0.0.1", port = 5500, debug = True)
+print(image)
